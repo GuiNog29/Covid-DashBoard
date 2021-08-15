6b4ebda52af9705731e223ed4281299a68ed68eb
Guilherme Almeida <guimathiasgui@gmail.com>
2021-08-15 18:21:08 -0300

    Refatorando código e separando em função separada

--- assets/js/http.js
+++
@@ -1,8 +0,0 @@
-function fetchJson(url) {
-  return fetch(url).then((r) => {
-    if(r.ok)
-      return r.json();
-    else
-      throw new Error(r.statusText);
-  });
-}
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,33 @@
-searchCountries = () => {
-  fetchJson("https://api.covid19api.com/countries").then((countries) => {
-    for (let country of countries) {
-      var combo = document.getElementById("combo");
-      const option = document.createElement("option");
-      option.textContent = country.Country;
-      combo.appendChild(option);
+(() => {
+  document.getElementById("combo");
+  document.getElementById("today");
+
+  (async () => {
+    let response = await Promise.allSettled([
+      fetch("https://api.covid19api.com/countries"),
+      fetch("https://api.covid19api.com/summary"),
+    ]);
+
+    if (response[0].status == "fulfilled") {
+      loadCountries(await response[0].value.json());
     }
-    });
-};
+  })();
+})();
 
-async function searchPrincipalInformations() {
-  let data = await fetchJson("https://api.covid19api.com/summary")
-  let date = new Date(data.Global.Date);
-  document.getElementById('tconfirmed').textContent = data.Global.TotalConfirmed;
-  document.getElementById('tdeath').textContent = data.Global.TotalDeaths;
-  document.getElementById('trecovered').textContent = data.Global.TotalRecovered;
-  document.getElementById("actives").textContent = 'Atualização';
-  document.getElementById('tactive').textContent = `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
-};
+function loadCountries(data) {
+  let combo = document.getElementById("combo");
 
-searchCountries();
-searchPrincipalInformations();
+  data.sort((a, b) => {
+    let x = a.Country.toUpperCase();
+    let y = b.Country.toUpperCase();
 
+    return x === y ? 0 : x > y ? 1 : -1;
+  });
 
-
+  for (index in data) {
+    combo.options[combo.options.length] = new Option(
+      data[index].Country,
+      data[index].Country
+    );
+  }
+}
